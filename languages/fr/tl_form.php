c8d2654b6ad08e3f02cb9104212c0f8c60659504
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-01-15 16:09:49 +0100

    Added french translation

---
+++ languages/fr/tl_form.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2011-2012
+ * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form']['leadEnabled']	= array('Enregistrer les données avec Leads', 'Enregistrer les données de ce formulaire avec Leads.');
+$GLOBALS['TL_LANG']['tl_form']['leadMaster']	= array('Configuration dans Leads', 'Déterminer s\'il s\'agit d\'un formulaire principal (langue principale utilisée aussi en BE) ou d\'un formulaire lié (autres langues).');
+$GLOBALS['TL_LANG']['tl_form']['leadMenuLabel']	= array('Libellé de navigation en backend', 'Saisissez un libellé personnalisé pour l\'affichage en backend. Laissez vide pour utiliser le nom du formulaire.');
+$GLOBALS['TL_LANG']['tl_form']['leadLabel']		= array('Champs de la liste en backend', 'Saisissez les noms des champs à afficher dans la liste BE entourés de doubles dièses (##champs##). Vous pouvez utiliser également du texte normal. Utilisez ##created## pour la date et l\'heure de création.');
+
+/**
+ * Other
+ */
+$GLOBALS['TL_LANG']['tl_form']['leadMasterBlankOptionLabel'] = 'Formulaire principal';
+
