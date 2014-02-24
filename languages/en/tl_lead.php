ed9e65eeacb407acb5cb52226344975a655a23b3
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-02-24 08:23:11 +0100

    Updated copyright statement

--- languages/en/tl_lead.php
+++ languages/en/tl_lead.php
@@ -1,29 +1,12 @@
 <?php
 
 /**
- * Contao Open Source CMS
- * Copyright (C) 2005-2012 Leo Feyer
+ * leads Extension for Contao Open Source CMS
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  terminal42 gmbh 2011-2012
- * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
- * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright  Copyright (c) 2011-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-leads
  */
 
 
@@ -51,4 +34,3 @@ $GLOBALS['TL_LANG']['tl_lead']['export_choice'] = array('export choice', '');
  * References
  */
 $GLOBALS['TL_LANG']['tl_lead']['label_format'] = 'Request from %s';
-
