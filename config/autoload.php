f82350887e0d38e781d3a5622d37e32af0cea733
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-02-24 08:20:20 +0100

    Added Contao 3 autoload files

---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * leads Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2011-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-leads
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    'Leads' => 'system/modules/leads/Leads.php',
+));
