382e231a5db14367c19cc47e391440283a2b7d97
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-09-09 10:37:10 +0200

    Added missing translations

---
+++ languages/fr/explain.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * leads Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2011-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-leads
+ */
+
+
+/**
+ * Insert tags
+ */
+$GLOBALS['TL_LANG']['XPL']['leadsTags'] = array
+(
+    array('##time##', 'Sera remplacé par l\'heure courante, ex. 10:15.'),
+    array('##date##', 'Sera remplacé par la date courante, ex. 03.09.2014.'),
+    array('##datim##', 'Sera remplacé par la date et l\'heure courante, ex. 03.09.2014 10:15.')
+);
