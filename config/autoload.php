108c0940daee5fcc759e9700cc00ea1ba2ffab2d
Yanick Witschi <yanick.witschi@terminal42.ch>
2015-07-13 13:11:30 +0200

    Copyright update

--- config/autoload.php
+++ config/autoload.php
@@ -3,7 +3,7 @@
 /**
  * leads Extension for Contao Open Source CMS
  *
- * @copyright  Copyright (c) 2011-2014, terminal42 gmbh
+ * @copyright  Copyright (c) 2011-2015, terminal42 gmbh
  * @author     terminal42 gmbh <info@terminal42.ch>
  * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
  * @link       http://github.com/terminal42/contao-leads
