103e4618e935b419d0f08365355f6dc9739a8776
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-01-23 22:28:36 +0100

    Updated manager plugin API

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -15,6 +15,7 @@ use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Config\ModuleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Terminal42\LeadsBundle\Terminal42LeadsBundle;
 
 class Plugin implements BundlePluginInterface
 {
@@ -24,7 +25,7 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            (new BundleConfig('Terminal42\LeadsBundle\Terminal42LeadsBundle'))
+            (new BundleConfig(Terminal42LeadsBundle::class))
                 ->setReplace(['leads'])
                 ->setLoadAfter(['haste', 'multicolumnwizard']),
             new ModuleConfig('haste'),
